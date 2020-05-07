6471d109c592f649e2ef3153143dda9b3f2ef2ff
deepanshu <deepanshu38841@gmail.com>
2020-05-07 16:26:10 +0530

    Created Main Screen, Added Cards and Icons

---
+++ lib/tools/store.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class Store {
+  String itemName;
+  double itemPrice;
+  String itemImage;
+  double itemRating;
+
+  Store.items({this.itemName, this.itemPrice, this.itemImage, this.itemRating});
+}
+
+List<Store> storeItems = [
+  Store.items(
+    itemName: "Pink Jeans",
+    itemPrice: 2540.0,
+    itemImage: "https://storage.needpix.com/rsynced_images/women-3063947_1280.png",
+    itemRating: 40.0
+  ),
+  Store.items(
+    itemName: "Black Strip White",
+    itemPrice: 6969.0,
+    itemImage: "https://storage.needpix.com/rsynced_images/women-3063947_1280.png",
+    itemRating: 40.0
+  ),
+  Store.items(
+    itemName: "Random cloth",
+    itemPrice: 6969.0,
+    itemImage: "https://storage.needpix.com/rsynced_images/women-3063947_1280.png",
+    itemRating: 40.0
+  ),
+  Store.items(
+    itemName: "Pink Shirt",
+    itemPrice: 2540.0,
+    itemImage: "https://storage.needpix.com/rsynced_images/women-3063947_1280.png",
+    itemRating: 40.0
+  ),
+  Store.items(
+    itemName: "Pink Shirt",
+    itemPrice: 2540.0,
+    itemImage: "https://storage.needpix.com/rsynced_images/women-3063947_1280.png",
+    itemRating: 40.0
+  ),
+  Store.items(
+    itemName: "Pink Shirt",
+    itemPrice: 2540.0,
+    itemImage: "https://storage.needpix.com/rsynced_images/women-3063947_1280.png",
+    itemRating: 40.0
+  ),
+  Store.items(
+    itemName: "Pink Shirt",
+    itemPrice: 2540.0,
+    itemImage: "https://storage.needpix.com/rsynced_images/women-3063947_1280.png",
+    itemRating: 40.0
+  ),
+  Store.items(
+    itemName: "Pink Shirt",
+    itemPrice: 2540.0,
+    itemImage: "https://storage.needpix.com/rsynced_images/women-3063947_1280.png",
+    itemRating: 40.0
+  ),
+  Store.items(
+    itemName: "Pink Shirt",
+    itemPrice: 2540.0,
+    itemImage: "https://storage.needpix.com/rsynced_images/women-3063947_1280.png",
+    itemRating: 40.0
+  ),
+  Store.items(
+    itemName: "Pink Shirt",
+    itemPrice: 2540.0,
+    itemImage: "https://storage.needpix.com/rsynced_images/women-3063947_1280.png",
+    itemRating: 40.0
+  ),
+
+];
--- lib/userScreens/myHomePage.dart
+++ lib/userScreens/myHomePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:shoppingapp/tools/store.dart';
 import 'favourites.dart';
 import 'messages.dart';
 import 'cart.dart';
@@ -9,6 +10,8 @@ import 'profile.dart';
 import 'delivery.dart';
 import 'about.dart';
 import 'loginLogout.dart';
+import 'package:shoppingapp/tools/store.dart';
+
 class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -27,10 +30,9 @@ class _MyHomePageState extends State<MyHomePage> {
           new IconButton(
               icon: new Icon(Icons.favorite),
               color: Colors.white,
-              onPressed: (){
+              onPressed: () {
                 Navigator.of(context).push(new CupertinoPageRoute(
-                    builder: (BuildContext context)  =>
-                    new MyFavourites()));
+                    builder: (BuildContext context) => new MyFavourites()));
               }),
           new Stack(
             alignment: Alignment.topLeft,
@@ -38,10 +40,9 @@ class _MyHomePageState extends State<MyHomePage> {
               new IconButton(
                   icon: new Icon(Icons.chat),
                   color: Colors.white,
-                  onPressed: (){
+                  onPressed: () {
                     Navigator.of(context).push(new CupertinoPageRoute(
-                        builder: (BuildContext context)  =>
-                        new MyMessages()));
+                        builder: (BuildContext context) => new MyMessages()));
                   }),
               new CircleAvatar(
                 radius: 8.0,
@@ -56,19 +57,109 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
       body: new Center(
-        child: new Text(
-          'My Home Page',
-          style: new TextStyle(fontSize: 20.0),
+        child: new Column(
+          children: <Widget>[
+            new Flexible(
+                child: new GridView.builder(
+              gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2),
+              itemCount: storeItems.length,
+              itemBuilder: (BuildContext context, int index) {
+                return new Card(
+                  child: Stack(
+                    alignment: FractionalOffset.topLeft,
+                    children: <Widget>[
+                      new Stack(
+                        alignment: FractionalOffset.bottomCenter,
+                        children: <Widget>[
+                          new Container(
+                            decoration: new BoxDecoration(
+                                image: new DecorationImage(
+                                    fit: BoxFit.fitWidth,
+                                    image: new NetworkImage(
+                                        storeItems[index].itemImage))),
+                          ),
+                          new Container(
+                            height: 35.0,
+                            color: Colors.black.withAlpha(100),
+                            child: new Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: new Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: <Widget>[
+                                  new Text(
+                                    "${storeItems[index].itemName.substring(0, 8)}...",
+                                    style: new TextStyle(
+                                        fontWeight: FontWeight.w700,
+                                        fontSize: 16.0,
+                                        color: Colors.white),
+                                  ),
+                                  new Text(
+                                    "N${storeItems[index].itemPrice}",
+                                    style: new TextStyle(
+                                        fontWeight: FontWeight.w700,
+                                        fontSize: 16.0,
+                                        color: Colors.red[500]),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          new Container(
+                            height: 40.0,
+                            width: 60.0,
+                            decoration: new BoxDecoration(
+                                color: Colors.black,
+                                borderRadius: new BorderRadius.only(
+                                  topRight: new Radius.circular(5.0),
+                                  bottomRight: new Radius.circular(5.0),
+                                )),
+                            child: new Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                new Icon(
+                                  Icons.star,
+                                  color: Colors.blue,
+                                  size: 20.0,
+                                ),
+                                new Text(
+                                  "${storeItems[index].itemRating}",
+                                  style: new TextStyle(color: Colors.white),
+                                )
+                              ],
+                            ),
+                          ),
+                          new IconButton(
+                              icon: Icon(
+                                Icons.favorite_border,
+                                color: Colors.blue,
+                              ),
+                              onPressed: (){
+
+                              })
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              },
+            ))
+          ],
         ),
       ),
       floatingActionButton: new Stack(
         alignment: Alignment.topLeft,
         children: <Widget>[
           new FloatingActionButton(
-            onPressed: (){
+            onPressed: () {
               Navigator.of(context).push(new CupertinoPageRoute(
-                  builder: (BuildContext context)  =>
-                  new MyCart()));
+                  builder: (BuildContext context) => new MyCart()));
             },
             child: new Icon(Icons.shopping_cart),
           ),
@@ -95,86 +186,90 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             new ListTile(
               leading: new CircleAvatar(
-                child: new Icon(Icons.notifications,
-                color: Colors.white,
-                size: 20.0,),
+                child: new Icon(
+                  Icons.notifications,
+                  color: Colors.white,
+                  size: 20.0,
+                ),
               ),
               title: new Text("Order Notifs"),
-              onTap: (){
+              onTap: () {
                 Navigator.of(context).push(new CupertinoPageRoute(
-                    builder: (BuildContext context)  =>
-                    new MyNotifications()));
+                    builder: (BuildContext context) => new MyNotifications()));
               },
             ),
             new ListTile(
               leading: new CircleAvatar(
-                child: new Icon(Icons.history,
-                color: Colors.white,
-                size: 20.0,),
+                child: new Icon(
+                  Icons.history,
+                  color: Colors.white,
+                  size: 20.0,
+                ),
               ),
               title: new Text("Order History"),
-              onTap: (){
+              onTap: () {
                 Navigator.of(context).push(new CupertinoPageRoute(
-                    builder: (BuildContext context)  =>
-                    new MyHistory()));
+                    builder: (BuildContext context) => new MyHistory()));
               },
             ),
             new Divider(),
             new ListTile(
               leading: new CircleAvatar(
-                child: new Icon(Icons.person,
-                color: Colors.white,
-                size: 20.0,),
+                child: new Icon(
+                  Icons.person,
+                  color: Colors.white,
+                  size: 20.0,
+                ),
               ),
               title: new Text("Profile Settings"),
-              onTap: (){
+              onTap: () {
                 Navigator.of(context).push(new CupertinoPageRoute(
-                    builder: (BuildContext context)  =>
-                    new MyProfile()));
+                    builder: (BuildContext context) => new MyProfile()));
               },
             ),
             new ListTile(
               leading: new CircleAvatar(
-                child: new Icon(Icons.home,
-                color: Colors.white,
-                size: 20.0,),
+                child: new Icon(
+                  Icons.home,
+                  color: Colors.white,
+                  size: 20.0,
+                ),
               ),
               title: new Text("Delivery Address"),
-              onTap: (){
+              onTap: () {
                 Navigator.of(context).push(new CupertinoPageRoute(
-                    builder: (BuildContext context)  =>
-                    new MyDelivery()));
+                    builder: (BuildContext context) => new MyDelivery()));
               },
             ),
             new Divider(),
             new ListTile(
               trailing: new CircleAvatar(
-                child: new Icon(Icons.help,
+                child: new Icon(
+                  Icons.help,
                   color: Colors.white,
-                  size: 20.0,),
+                  size: 20.0,
+                ),
               ),
               title: new Text("About US"),
-              onTap: (){
+              onTap: () {
                 Navigator.of(context).push(new CupertinoPageRoute(
-                    builder: (BuildContext context)  =>
-                    new AboutUs()));
+                    builder: (BuildContext context) => new AboutUs()));
               },
             ),
             new ListTile(
               trailing: new CircleAvatar(
-                child: new Icon(Icons.exit_to_app,
+                child: new Icon(
+                  Icons.exit_to_app,
                   color: Colors.white,
-                  size: 20.0,),
+                  size: 20.0,
+                ),
               ),
               title: new Text("Login"),
-              onTap: (){
+              onTap: () {
                 Navigator.of(context).push(new CupertinoPageRoute(
-                    builder: (BuildContext context)  =>
-                    new MyLogin()));
+                    builder: (BuildContext context) => new MyLogin()));
               },
             ),
-
-
           ],
         ),
       ),
