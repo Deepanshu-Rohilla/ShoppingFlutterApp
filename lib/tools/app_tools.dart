693a10c51eb382d17865848853ff6ba70cca83f6
deepanshu <deepanshu38841@gmail.com>
2020-05-09 14:20:54 +0530

    Made Login Page

--- lib/tools/app_tools.dart
+++ lib/tools/app_tools.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-Widget appTextField({Icon textIcon, String textHint, bool isPassword, double sidePadding ,TextEditingController controller}){
+Widget appTextField({IconData textIcon, String textHint, bool isPassword, double sidePadding ,TextEditingController controller}){
+  sidePadding ==null ? sidePadding = 0.0 : sidePadding = sidePadding;
+  textHint == null ? textHint = "" : textHint = textHint;
   return Padding(
     padding: new EdgeInsets.only(left: sidePadding),
     child: new Container(
@@ -12,10 +14,38 @@ Widget appTextField({Icon textIcon, String textHint, bool isPassword, double sid
         controller: controller,
         obscureText: isPassword == null ? false : isPassword,
         decoration: new InputDecoration(
-          hintText: textHint==null ? "" : textHint,
-          prefixIcon: textIcon==null ? new Container() : new Icon(Icons.person)
+            border: InputBorder.none,
+          hintText: textHint,
+          prefixIcon: textIcon==null ? new Container() : new Icon(textIcon)
         ),
       ),
     ),
   );
 }
+Widget appButton(String buttonText, double buttonPadding, Color buttonColour,){
+  buttonText == null ? buttonText = "App button" : buttonText = buttonText;
+  buttonPadding ==null ? buttonPadding = 0.0 : buttonPadding = buttonPadding;
+  buttonColour ==null ? buttonColour = Colors.black : buttonColour = buttonColour;
+
+
+  return Padding(
+    padding: new EdgeInsets.all(buttonPadding),
+    child: new RaisedButton(onPressed: (){},
+        color: Colors.white,
+        shape: new RoundedRectangleBorder(
+            borderRadius: new BorderRadius.all(new Radius.circular(15.0)) ),
+
+        child: Container(
+          height: 50.0,
+          child: new Center(
+            child: new Text(buttonText,
+            style: new TextStyle(
+              color: buttonColour,
+              fontSize: 10.0,
+            ),),
+          ),
+        )
+    ),
+  );
+}
+
--- lib/userScreens/loginLogout.dart
+++ lib/userScreens/loginLogout.dart
@@ -8,21 +8,51 @@ class MyLogin extends StatefulWidget {
 }
 
 class _MyLoginState extends State<MyLogin> {
+  TextEditingController email = new TextEditingController();
+  TextEditingController password = new TextEditingController();
+  final scaffoldKey = new GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      backgroundColor: Theme.of(context).primaryColor,
-      appBar: new AppBar(
-        title: new Text("Login"),
-        centerTitle: false,
-      ),
-      body: new SingleChildScrollView(
-        child: new Column(
-          children: <Widget>[
-            appTextField()
-          ],
+        backgroundColor: Theme.of(context).primaryColor,
+        appBar: new AppBar(
+          title: new Text("Login"),
+          centerTitle: false,
         ),
-      )
-    );
+        body: new SingleChildScrollView(
+          child: new Column(
+            children: <Widget>[
+              new SizedBox(
+                height: 30.0,
+              ),
+              appTextField(
+                isPassword: false,
+                sidePadding: 10.0,
+                textHint: "Email Address",
+                textIcon: Icons.email,
+                controller: email,
+              ),
+              new SizedBox(
+                height: 30.0,
+              ),
+              appTextField(
+                isPassword: true,
+                sidePadding: 10.0,
+                textHint: "Password",
+                textIcon: Icons.lock,
+                controller: password,
+              ),
+              appButton("Login", 20.0, Theme.of(context).primaryColor, on),
+              new GestureDetector(
+                onTap: (){
+
+                },
+                  child: new Text("New User? Sign Up here"))
+            ],
+          ),
+        ));
   }
+
+
 }
